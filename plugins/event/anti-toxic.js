9ac306d8e4e3bc984a23e70d9d7ead3380df6832
danzprstt <gamelegend1092@gmail.com>
2025-02-17 22:25:27 +0700

    Update anti-toxic.js

--- plugins/event/anti-toxic.js
+++ plugins/event/anti-toxic.js
@@ -1,4 +1,4 @@
-const toxic = ['kontol', 'anj', 'kntl', 'anjing', 'bngst', 'bgst', 'bangsat', 'memek', 'mmk', 'njir', 'najis', 'anjir', 'jing', 'njing', 'kntl', 'kontol', 'babi', 'monyet', 'mnyt', 'jembut', 'jmbt', 'lol', 'tolol'];
+const toxic = ['kontol', 'anj', 'anjing', 'bngst', 'bgst', 'bangsat', 'memek', 'mmk', 'njir', 'najis', 'anjir', 'jing', 'njing', 'kntl', 'kontol', 'babi', 'monyet', 'mnyt', 'jembut', 'jmbt', 'lol', 'tolol', 'goblok', 'goblk', 'gblk', 'ngentot', 'ngntt', 'ngentt', 'ngen', 'kocak', 'kck', 'peju', 'jelek', 'jlk'];
 const agree = ['lanjut', 'loli']
 module.exports = {
    start: async (m, {
@@ -9,7 +9,7 @@ module.exports = {
          if ((!m.isGroup || db.chats[m.chat].antiToxic) && toxic.some(word => budy.toLowerCase().includes(word))) {
             if (agree.some(v => budy.toLowerCase().includes(v))) return
             if (m.fromMe || m.isBaileys) return
-            conn.adReply(m.chat, `Hey Ketikannya!`, cover, m);
+            conn.adReply(m.chat, `WOOOYYY, LU SEKALI LAGI KETIKANNYA KEK GITU GUA GABLOGIN LUHH!`, cover, m);
          }
       } catch (e) {
          return false
