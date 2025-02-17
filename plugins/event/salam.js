08e36bd45278288ed5d58678f4b34e640dce3b96
danzprstt <gamelegend1092@gmail.com>
2025-02-18 04:55:44 +0700

    Update salam.js

--- plugins/event/salam.js
+++ plugins/event/salam.js
@@ -5,11 +5,11 @@ module.exports = {
       User,
       Format
    }) => {
-       //	// Salam
-		if (/^a(s|ss)alamu('|)alaikum(| )(wr|)( |)(wb|)$/.test(budy?.toLowerCase())) {
-			const jwb_salam = ['Wa\'alaikumusalam','Wa\'alaikumusalam wr wb','Wa\'alaikumusalam Warohmatulahi Wabarokatuh']
-			m.reply(pickRandom(jwb_salam))
-		}
+       // Salam
+	if (/^a(s|ss)alamu('|)alaikum(| )(wr|)( |)(wb|)$/.test(budy?.toLowerCase())) {
+	const jwb_salam = ['Wa\'alaikumusalam','Wa\'alaikumusalam wr wb','Wa\'alaikumusalam Warohmatulahi Wabarokatuh']
+	m.reply(pickRandom(jwb_salam))
+		
 		
    }
 };
