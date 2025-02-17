654da11713314cc710f3ec43619e90916037841d
danzprstt <gamelegend1092@gmail.com>
2025-02-18 04:32:18 +0700

    Update group-listonline.js

--- plugins/group/group-listonline.js
+++ plugins/group/group-listonline.js
@@ -2,19 +2,14 @@ exports.default = {
    names: ['menu group'],
    tags: ['listonline'],
    command: ['lo', 'listonline'],
-   start: async (m, {
+   st m, {
       conn,
       text,
       participants
    }) => {
-      let mem = [];
-case 'listonline': case 'liston': {
-				if (!m.isGroup) return m.reply(mess.group)
-				let id = args && /\d+\-\d+@g.us/.test(args[0]) ? args[0] : m.chat
-				if (!store.presences || !store.presences[id]) return m.reply('Sedang Tidak ada yang online!')
-				let online = [...Object.keys(store.presences[id]), botNumber]
-				await m.reply('List Online:\n\n' + online.map(v => setv + ' @' + v.replace(/@.+/, '')).join`\n`, { mentions: online }).catch((e) => m.reply('Sedang Tidak Ada Yang Online..'))
-			}
+      let mem = [];{
+ case 'listonline': case 'liston': {
+ case 'listonline'
          showAds: true,
          mentions: mem
       })
